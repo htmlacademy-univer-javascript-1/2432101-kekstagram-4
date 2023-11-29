f58e997d211bba1bc2a2d78ddacedff9dc690804
Keks <keks@htmlacademy.ru>
2023-11-29 20:07:50 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/create-photos.js
@@ -0,0 +1,28 @@
+import {
+  MESSAGES, NAMES, DESCRIPTIONS, CommentsCount,
+  LikesCount, MessagesCount, AvatarId, COUNT_PHOTOS
+} from './constants.js';
+
+import { getRandomNumberFromInterval, getId, createRandomIdFromRangeGenerator, shuffle } from './util.js';
+
+const generatePhotoID = createRandomIdFromRangeGenerator(1, COUNT_PHOTOS);
+const generateCommentID = getId();
+
+const getComment = (id) => ({
+  id: id,
+  avatar: `img/avatar-${getRandomNumberFromInterval(AvatarId.MIN, AvatarId.MAX)}.svg`,
+  message: shuffle(MESSAGES).slice(0, getRandomNumberFromInterval(MessagesCount.MIN, MessagesCount.MAX)),
+  name: NAMES[getRandomNumberFromInterval(0, NAMES.length - 1)],
+});
+
+const getPhotoData = (id) => ({
+  id: id,
+  url: `photos/${id}.jpg`,
+  likes: getRandomNumberFromInterval(LikesCount.MIN, LikesCount.MAX),
+  description: DESCRIPTIONS[getRandomNumberFromInterval(0, DESCRIPTIONS.length - 1)],
+  comments: Array.from({ length: getRandomNumberFromInterval(CommentsCount.MIN, CommentsCount.MAX) }, () => getComment(generateCommentID())),
+});
+
+const getPhotos = () => Array.from({ length: COUNT_PHOTOS }, () => getPhotoData(generatePhotoID()));
+
+export { getPhotos };
---
+++ 7/js/util.js
@@ -0,0 +1,34 @@
+const getRandomNumberFromInterval = (start, end) =>
+  Math.ceil(Math.random() * (end - start + 1)) + (start - 1);
+
+function createRandomIdFromRangeGenerator(min, max) {
+  const previousValues = [];
+
+  return function () {
+    let currentValue = getRandomNumberFromInterval(min, max);
+
+    if (previousValues.length >= (max - min + 1)) {
+      return null;
+    }
+
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomNumberFromInterval(min, max);
+    }
+
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+}
+
+const getId = () => {
+  let lastGeneratedId = 0;
+
+  return function () {
+    lastGeneratedId++;
+    return lastGeneratedId;
+  };
+};
+
+const shuffle = (array) => array.sort(() => Math.random() - 0.5);
+
+export { getRandomNumberFromInterval, getId, shuffle, createRandomIdFromRangeGenerator };
